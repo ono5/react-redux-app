a4f49819b1180b6c9d719a046386d02ccaaecaaa
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-11-29 21:46:33 +0900

    Implement React Form input tag

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -9,7 +9,10 @@ import { Link } from 'react-router-dom';
 class EventsNew extends Component {
   renderField(field) {
     const { input, label, type, meta: {touched, error}} = field
-    return (<div></div>)
+    return (<div>
+              <input {...input} placeholder={label} type={type} />   
+              {touched && error && <span>{error}</span>}
+            </div>)
   }
   render() {
     return (
